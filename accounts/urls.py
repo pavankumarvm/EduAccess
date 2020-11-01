c3829bdd6f2506c94b22fcea5138a645f5bb4d33
Suryakant <suryakantk94@gmail.com>
2020-11-01 23:01:03 +0530

    All tabs added Navbar

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('login/', views.login_user, name="login_user"),
     path('logout/', views.logout_user, name="logout_user"),
     path('feedback/', views.feedback_user, name="feedback_user"),
+    path('stream/', views.stream_user, name="stream_user"),
+    path('college/', views.college_user, name="college_user"),
+    path('manageclg/', views.manageclg_user, name="manageclg_user"),
+    path('training/', views.training_user, name="training_user"),
+    path('viewstd/', views.viewstd_user, name="viewstd_user"),
+    path('questions/', views.questions_user, name="questions_user"),
+    path('forgotpass/', views.forgotpass_user, name="forgotpass_user"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -90,3 +90,38 @@ def feedback_user(request):
     return render(request,
                     'feedback.html',
                 )
+
+def stream_user(request):
+    return render(request,
+                    'StreamDetails.html',
+                )
+
+def college_user(request):
+    return render(request,
+                    'clgdetails.html',
+                )                
+
+def manageclg_user(request):
+    return render(request,
+                    'ManageClg.html',
+                )                
+
+def training_user(request):
+    return render(request,
+                    'TrainingData.html',
+                )   
+
+def viewstd_user(request):
+    return render(request,
+                    'ViewStudents.html',
+                )                              
+
+def questions_user(request):
+    return render(request,
+                    'Questions.html',
+                )        
+                
+def forgotpass_user(request):
+    return render(request,
+                    'forgotpass.html',
+                )                     
